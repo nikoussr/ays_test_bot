b1e04ed1a8d25a2a05453876ee7a3497082fd92d
Захар <zog1@tpu.ru>
2024-06-18 17:23:22 +0700

    починка должностей+

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
-#TOKEN = '7003566850:AAGo57-I_ZT0y2elh9tS-oSNFyHpvb6TB70'
-TOKEN = '7376693418:AAHeqR1_J2d50dutBywQqaiMcnd7SSMlseE'
+TOKEN = '7003566850:AAGo57-I_ZT0y2elh9tS-oSNFyHpvb6TB70'
+#TOKEN = '7376693418:AAHeqR1_J2d50dutBywQqaiMcnd7SSMlseE'
 ADMIN = ['637403771', '695088267', '243929098']
 #JOBS = ['1_Менеджер', '2_Официант', '3_Бармен', '4_Старший бармен', '5_Администратор ресепшен', '6_Повар']
 JOBS = {
